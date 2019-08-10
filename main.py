7288b099aeb096b54f946c9ee6e716a1514c7a80
Rick <menneric84@gmail.com>
2019-08-09 19:29:59 -0500

    need to remove old turns

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ RED = (255,0,0)
 GREEN = (0, 255, 0)
 SIZE = 50
 def getNewFood():
-    global food, snake
-
+    global food, snake, win
     if food == None or (snake.head.x == food.x and snake.head.y == food.y):
         food = Cube(random.randint(1,9) * SIZE, random.randint(1,9) * SIZE, "NONE")
         if food != None:
-            snake.
-        print("Food x:" + str(food.x) + " Food y:" + str(food.y))
-def drawGrid(win):
+            snake.add(win)
+def drawGrid():
+    global win
     x = 50
     y = 0
     while x < 500:
@@ -31,22 +30,24 @@ def drawGrid(win):
         pygame.draw.line(win, WHITE, (0,y), (500,y))
         y += 50
 
-def redraw(win):
+def redraw():
+    global win
     win.fill((0,0,0))  # Fills the screen with black
     getNewFood()
     food.draw(win,RED)
-    drawGrid(win)  # Will draw our grid lines
+    drawGrid()  # Will draw our grid lines
     snake.move(win)
     pygame.display.update()  # Updates the screen
 
 def main():
-    global snake, food
+    global snake, food, win
+    bodyFlag = False
     snake = Snake(0,0)
     food = Cube(random.randint(1,10) * SIZE, random.randint(1,10) * SIZE, "NONE")
     pygame.init()
     win = pygame.display.set_mode((500, 500))
     clock = pygame.time.Clock() # creating a clock object
-    redraw(win)
+    redraw()
     run = True
     while run:
         pygame.time.delay(100)
@@ -54,7 +55,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        redraw(win)
+        redraw()
 
     
 
--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ LEFT = "left"
 DOWN = "down"
 UP = "up"
 GREEN = (0,255,255)
+
 class Snake:
     body = []
     turns = []
@@ -16,10 +17,19 @@ class Snake:
         self.head = Cube(x,y, RIGHT)
         self.body.append(self.head)
 
-    def add(self)
+    def add(self, win):
+        d = self.body[-1]
+        if d.direction == RIGHT:
+            temp = Cube(d.x - 50, d.y, d.direction)
+        if d.direction == LEFT:
+            temp = Cube(d.x + 50, d.y, d.direction) 
+        if d.direction == UP:
+            temp = Cube(d.x, d.y + 50, d.direction) 
+        if d.direction == DOWN:
+            temp = Cube(d.x, d.y - 50, d.direction)
+        self.body.append(temp)
     def move(self, win):
         keys = pygame.key.get_pressed()
-
         for key in keys:
             if keys[pygame.K_LEFT] and self.head.direction != RIGHT:
                 self.head.direction = LEFT
@@ -33,14 +43,21 @@ class Snake:
             if keys[pygame.K_DOWN] and self.head.direction != UP:
                 self.head.direction = DOWN
                 self.turns.append(Cube(self.head.x, self.head.y, self.head.direction))
-        if self.head.direction == LEFT:
-            self.head.x -= 50
-        if self.head.direction == RIGHT:
-            self.head.x += 50
-        if self.head.direction == UP:
-            self.head.y -= 50
-        if self.head.direction == DOWN:
-            self.head.y += 50
+        for c in self.body:
+            for t in self.turns:
+                if t.x == c.x and t.y == c.y:
+                    c.direction = t.direction 
 
-        pygame.draw.rect(win, GREEN, (self.head.x, self.head.y, 50, 50))
+        
+
+        for c in self.body:
+            if c.direction == LEFT:
+                c.x -= 50
+            if c.direction == RIGHT:
+                c.x += 50
+            if c.direction == UP:
+                c.y -= 50
+            if c.direction == DOWN:
+                c.y += 50
+            pygame.draw.rect(win, GREEN, (c.x, c.y, 50, 50))
             
